0e2d614dade12f513f260b40a269ea72c76d15b8
Marina48Lip <115417922+Marina48Lip@users.noreply.github.com>
2023-10-13 21:20:08 +0300

    Create module.py

---
+++ DZ4/module.py
@@ -0,0 +1,40 @@
+import time
+import yaml
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+with open('config.yaml') as f:
+    testdata = yaml.safe_load(f)
+
+service = Service(testdata['driver_path'])
+options = webdriver.ChromeOptions()
+
+class Site:
+    def __init__(self, address):
+        self.driver = webdriver.Chrome(service=service, options=options)
+        self.driver.implicitly_wait(10)
+        self.driver.maximize_window()
+        self.driver.get(address)
+        time.sleep(testdata['sleep_time'])
+
+    def find_element(self, mode, path):
+
+        if mode == 'css':
+            element = self.driver.find_element(By.CSS_SELECTOR, path)
+        elif mode == 'xpath':
+            element = self.driver.find_element(By.XPATH, path)
+        else:
+            element = None
+        return  element
+
+    def get_element_property(self, mode, path, property):
+        element = self.find_element(mode, path)
+        return element.value_of_css_property(property)
+
+    def close(self):
+        self.driver.close()
+
+
+
+
